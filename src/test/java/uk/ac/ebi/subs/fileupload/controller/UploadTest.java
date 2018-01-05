473e11d5f09a9d3208cba1cc81dbb03bd1f709be
Karoly Erdos <karoly@ebi.ac.uk>
2018-01-05 11:02:31 +0000

    Uploading the same file 10 times and calculate the average time to upload

--- src/test/java/uk/ac/ebi/subs/fileupload/controller/UploadTest.java
+++ src/test/java/uk/ac/ebi/subs/fileupload/controller/UploadTest.java
@@ -1,7 +1,5 @@
 package uk.ac.ebi.subs.fileupload.controller;
 
-import io.tus.java.client.TusUpload;
-import io.tus.java.client.TusUploader;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,28 +11,20 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-import uk.ac.ebi.subs.fileupload.PostFileDelete;
-import uk.ac.ebi.subs.fileupload.TerminatedUploadExecutor;
 import uk.ac.ebi.subs.fileupload.UploadClient;
-import uk.ac.ebi.subs.fileupload.UploadExecutor;
 
 import java.io.File;
 import java.net.URI;
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.hamcrest.Matchers.lessThanOrEqualTo;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
 
 @RunWith(value = SpringRunner.class)
 @SpringBootTest
 @WebAppConfiguration
-
 public class UploadTest {
 
     private static final Logger LOGGER = Logger.getLogger( UploadTest.class.getName() );
@@ -45,37 +35,43 @@ public class UploadTest {
     File file;
     String submissionId = "12345-67890";
 
+    private List<Long> measuredTimes;
+
     @Before
     public void setup() {
         this.restTemplate = new RestTemplate();
 
-        file = new File("src/test/resources/ReactiveKafka.mp4");
+        file = new File("src/test/resources/guggenheim_museum.jpg");
 
         headers = new HashMap<>();
         headers.put("submissionID", submissionId);
         headers.put("filename", file.getName());
+
+        measuredTimes = new ArrayList<>();
     }
 
     @Test
     public void canUploadAFile() throws Exception {
-        UploadClient uploadClient = new UploadClient();
+        for (int i = 0; i < 10; i++) {
+            UploadClient uploadClient = new UploadClient();
+            uploadClient.setFile(file, headers);
 
-        uploadClient.setFile(file, headers);
+            long startTime = Instant.now().toEpochMilli();
 
-        long startTime = Instant.now().toEpochMilli();
+            uploadClient.attemptUpload();
 
-        uploadClient.attemptUpload();
+            long endTime = Instant.now().toEpochMilli();
+            measuredTimes.add(endTime - startTime);
+        }
 
-        long endTime = Instant.now().toEpochMilli();
-        long output = endTime - startTime;
+//        final UploadExecutor executor = uploadClient.getExecutor();
+//        TusUploader uploader = executor.getUploader();
+//        URI uploadURI = uploader.getUploadURL().toURI();
 
-        final UploadExecutor executor = uploadClient.getExecutor();
-        TusUploader uploader = executor.getUploader();
-        URI uploadURI = uploader.getUploadURL().toURI();
+//        assertThat(checkFileExsistence(uploadURI), is(equalTo(HttpStatus.OK)));
 
-        assertThat(checkFileExsistence(uploadURI), is(equalTo(HttpStatus.OK)));
-
-        LOGGER.info("Elapsed time in milliseconds: " + output);
+        LOGGER.info("Average elapsed time in milliseconds: "
+                + measuredTimes.stream().mapToLong(time -> time).average().getAsDouble());
     }
 
     private HttpStatus checkFileExsistence(URI uploadURI) {
